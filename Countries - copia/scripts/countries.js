d86466026cdd494d3e16abc6b086007cbafd2054
Andrea gaona 14 <andreitagaona05@gmail.com>
2024-11-09 10:24:09 -0500

    Countries.js

--- Countries - copia/scripts/countries.js
+++ Countries - copia/scripts/countries.js
@@ -152,4 +152,12 @@ export const countries = [
       language: "Spanish",
       flag: "assets/images/flags/Venezuela.png"
   }
+  {
+    name: "Venezuela",
+    capital: "Caracas",
+    population: 28435943,
+    currency: "Bolívar",
+    language: "Spanish",
+    flag: "assets/images/flags/Venezuela.png"
+}
 ];
